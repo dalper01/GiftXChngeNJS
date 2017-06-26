6a48803191bcb41cd7570a61dff7bf5fab41d3ec
dalper01 <alperdave@gmail.com>
2017-06-26 00:52:02 -0400

    no cache

---
+++ modules/returns/client/services/returns.client.service.customerReturn.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+
+//Product service used for communicating with the articles REST endpoints
+angular.module('returns').factory('customerReturnService', ['$resource', '$http', '$q',
+    function ($resource, $http, $q) {
+        
+        // url's for http call -- should be centralized
+        var constants = {
+            uriSearchKeyword: '/api/keywordsearch/',
+            uriSearchKeywordTest: '/api/bditemsearch/',
+            uriSearchProvideUPC: ''
+        };
+        
+        // product search response repo        
+        var _customerReturn = {
+            returnItems: []
+        };
+        
+        
+        // service object with data setters and getters
+        var customerReturnService = {
+            getCustomerReturns: function () {
+                return _customerReturn;
+            },
+            setCustomerReturns: function (data) {
+                angular.copy(data, _customerReturn);
+            }
+        };
+        
+        customerReturnService.addReturnItem = function (item) {
+            _customerReturn.returnItems.push(item);
+        }
+        
+        customerReturnService.removeReturnItem = function (item) {
+            _customerReturn.returnItems.push(item);
+        }
+        
+        
+        
+        // Search for product-items by keyword descriptions and filters        
+        customerReturnService.findKeyword = function (NewUPC) {
+            var deferred = $q.defer();
+            
+            $http({
+                url: constants.uriSearchKeyword + encodeURIComponent(NewUPC),
+                method: "GET"
+                //params: { keyword: $scope.NewUPC }
+            })
+            .success(function (data, status, headers, config) {
+                //setProductSearch() -- refactor
+                angular.copy(data.data, _productSearch);
+                deferred.resolve(data);
+            })
+            .error(function (error) {
+                deferred.reject(error);
+            });
+            
+            return deferred.promise;
+        }
+        
+        return customerReturnService;
+
+    }]);
---
+++ modules/returns/client/services/returns.client.service.searchProduct.js
@@ -0,0 +1,52 @@
+﻿'use strict';
+
+//Product service used for communicating with the articles REST endpoints
+angular.module('returns').factory('searchProduct', ['$resource', '$http', '$q',
+    function ($resource, $http, $q) {
+        
+        // url's for http call -- should be centralized
+        var constants = {
+            uriSearchKeyword: '/api/keywordsearch/',
+            uriSearchKeywordTest: '/api/bditemsearch/',
+            uriSearchProvideUPC: ''
+        };
+        
+        // product search response repo        
+        var _productSearch = {};
+        
+        
+        // service object with data setters and getters
+        var returnService = {
+            getProductSearch: function () {
+                return _productSearch;
+            },
+            setProductSearch: function (data) {
+                angular.copy(data, _productSearch);
+            }
+        };
+        
+        
+        // Search for product-items by keyword descriptions and filters        
+        returnService.findKeyword = function (NewUPC) {
+            var deferred = $q.defer();
+            
+            $http({
+                url: constants.uriSearchKeyword + encodeURIComponent(NewUPC),
+                method: "GET"
+                //params: { keyword: $scope.NewUPC }
+            })
+            .success(function (data, status, headers, config) {
+                //setProductSearch() -- refactor
+                angular.copy(data.data, _productSearch);
+                deferred.resolve(data);
+            })
+            .error(function (error) {
+                deferred.reject(error);
+            });
+            
+            return deferred.promise;
+        }
+
+        return returnService;
+
+    }]);
