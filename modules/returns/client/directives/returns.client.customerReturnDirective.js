95941ff89a558653ab3230342a21731c39f93233
dalper01 <alperdave@gmail.com>
2017-06-26 01:19:01 -0400

    left stuff

---
+++ modules/returns/client/directives/returns.client.customerReturnDirective.js
@@ -0,0 +1,12 @@
+﻿angular.module('returns').directive('customerReturn', [function ($compile) {
+        return {
+            restrict: "AE",
+            //controller: 'navController',
+            //scope: {
+            //    settab: '=',
+            //    updateFn: '&'
+            //},
+            
+            templateUrl: "/modules/returns/client/views/returns.client.customerReturnView.html"
+        }
+    }]);
