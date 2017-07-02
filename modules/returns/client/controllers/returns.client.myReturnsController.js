e96e4d7ad12266397f1c5db67b25b64a92939ebd
dalper01 <alperdave@gmail.com>
2017-07-02 00:12:47 -0400

    adding controller to source control

---
+++ modules/returns/client/controllers/returns.client.myReturnsController.js
@@ -0,0 +1,33 @@
+﻿angular.module('returns').controller('MyReturnsController', ['$http', '$scope', '$state', '$stateParams', '$location', 'Authentication', 'customerReturnService', 'searchProductService',
+    function ($http, $scope, $state, $stateParams, $location, Authentication, customerReturnService, searchProductService) {
+    	$scope.user = Authentication.user;
+        
+        $scope.dateFormat = function (date) {
+            var date = new Date(date);
+            var year = date.getFullYear();
+            ///// Add 1 because JavaScript months start at 0
+            var month = (1 + date.getMonth()).toString();
+            month = month.length > 1 ? month : '0' + month;
+            var day = date.getDate().toString();
+            day = day.length > 1 ? day : '0' + day;
+            return month + '/' + day + '/' + year;
+            //return 'hello';
+        }
+        $http({
+            //url: constants.uriReturns,
+            url: 'api/returns',
+            method: "GET"
+                //params: { keyword: $scope.NewUPC }
+        })
+            .success(function (data, status, headers, config) {
+            //angular.copy(data.data, _productSearch);
+            $scope.myReturns = data;
+            //deferred.resolve(data);
+        })
+            .error(function (error) {
+            deferred.reject(error);
+        });
+
+
+
+    }]);
